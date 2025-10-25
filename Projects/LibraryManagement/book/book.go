6b2affc3a5157d8ea82b6c0445906375a0c3208b
Navnath Kadam <navnathkadam284@gmail.com>
2025-10-26 02:27:39 +0530

    refacor: added comments in project 2

--- Projects/LibraryManagement/book/book.go
+++ Projects/LibraryManagement/book/book.go
@@ -1,3 +1,4 @@
+// Package book handles book-related functionality
 package book
 
 import (
@@ -5,23 +6,30 @@ import (
 	"fmt"
 )
 
+// AllBooks stores a global list of all books in the library
 var AllBooks []string
 
+// Book represents a book with its author and list of book titles
 type Book struct {
-	Author string
-	Books  []string
+	Author string   // Name of the book's author
+	Books  []string // List of books by this author
 }
 
+// AddBooks adds multiple books to the library and displays logs
+// It accepts a variadic parameter of book titles to add
 func (b *Book) AddBooks(books ...string) {
+	// Add each book to the global AllBooks list
 	for _, v := range books {
 		AllBooks = append(AllBooks, v)
 	}
 
+	// Display all user logs
 	totalLogs := user.Logs
 	for _, v := range totalLogs {
 		fmt.Println(v)
 	}
 
+	// Display all books being added
 	fmt.Println("Total Books")
 	for _, v := range books {
 		fmt.Println(v)
